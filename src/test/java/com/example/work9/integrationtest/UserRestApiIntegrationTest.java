38c99fd4a7b6ac5440980ecbdf64d71808863e94
maki <mkr.fb-66@cameo.plala.or.jp>
2023-06-23 22:01:09 +0900

    GET処理の結合テスト：timestampを固定化して返す処理に変更

--- src/test/java/com/example/work9/integrationtest/UserRestApiIntegrationTest.java
+++ src/test/java/com/example/work9/integrationtest/UserRestApiIntegrationTest.java
@@ -3,10 +3,9 @@ package com.example.work9.integrationtest;
 import com.github.database.rider.core.api.dataset.DataSet;
 import com.github.database.rider.spring.api.DBRider;
 import org.junit.jupiter.api.Test;
-import org.skyscreamer.jsonassert.Customization;
+import org.mockito.MockedStatic;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
-import org.skyscreamer.jsonassert.comparator.CustomComparator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -17,6 +16,10 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.nio.charset.StandardCharsets;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+import static org.mockito.Mockito.mockStatic;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -36,19 +39,20 @@ class UserRestApiIntegrationTest {
                 .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
         JSONAssert.assertEquals("""
                 [
-                {
-                "id": 1,
-                "name": "tanaka"
-                },
-                {
-                "id": 2,
-                "name": "yamada"
-                },
-                {
-                "id": 3,
-                "name": "suzuki"
-                }
-                ]""", response, JSONCompareMode.STRICT);
+                   {
+                      "id":1,
+                      "name":"tanaka"
+                   },
+                   {
+                      "id":2,
+                      "name":"yamada"
+                   },
+                   {
+                      "id":3,
+                      "name":"suzuki"
+                   }
+                ]
+                """, response, JSONCompareMode.STRICT);
     }
 
     @Test
@@ -72,8 +76,8 @@ class UserRestApiIntegrationTest {
                 .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
         JSONAssert.assertEquals("""
                 {
-                "id": 1,
-                "name": "tanaka"
+                   "id":1,
+                   "name":"tanaka"
                 }
                 """, response, JSONCompareMode.STRICT);
     }
@@ -82,18 +86,25 @@ class UserRestApiIntegrationTest {
     @DataSet(value = "datasets/names.yml")
     @Transactional
     void 指定したidのユーザーが存在しない場合ステータス404を返すこと() throws Exception {
-        String response = mockMvc.perform(MockMvcRequestBuilders.get("/users/99"))
-                .andExpect(MockMvcResultMatchers.status().isNotFound())
-                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
-        JSONAssert.assertEquals("""
-                {          
-                "timestamp": "",
-                "status": "404",
-                "error": "Not Found",
-                "message": "No user found for id: 99",
-                "path": "/users/99"
-                }
-                """, response, new CustomComparator(JSONCompareMode.STRICT,
-                new Customization("timestamp", ((expectedTimestamp, actualTimestamp) -> true))));
+        // ZoneDateTime.nowを固定化する
+        ZonedDateTime fixedDateTime = ZonedDateTime.of(
+                2023, 6, 23, 21, 0, 0, 0,
+                ZoneId.systemDefault());
+
+        try (MockedStatic<ZonedDateTime> mockedStatic = mockStatic(ZonedDateTime.class)) {
+            mockedStatic.when(ZonedDateTime::now).thenReturn(fixedDateTime);
+            String response = mockMvc.perform(MockMvcRequestBuilders.get("/users/99"))
+                    .andExpect(MockMvcResultMatchers.status().isNotFound())
+                    .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+            JSONAssert.assertEquals("""
+                    {
+                       "timestamp":"2023-06-23T21:00+09:00[Asia/Tokyo]",
+                       "status":"404",
+                       "error":"Not Found",
+                       "message":"No user found for id: 99",
+                       "path":"/users/99"
+                    }
+                    """, response, JSONCompareMode.STRICT);
+        }
     }
 }
