c62e4dfb15b3a85b1b798744bda77fed454dbf52
maki <mkr.fb-66@cameo.plala.or.jp>
2023-06-25 22:25:52 +0900

    PATCH,DELETE処理の結合テスト実装

--- src/test/java/com/example/work9/integrationtest/UserRestApiIntegrationTest.java
+++ src/test/java/com/example/work9/integrationtest/UserRestApiIntegrationTest.java
@@ -22,6 +22,7 @@ import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
 import static org.mockito.Mockito.mockStatic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -87,8 +88,6 @@ class UserRestApiIntegrationTest {
     }
 
     @Test
-    @DataSet(value = "datasets/names.yml")
-    @Transactional
     void 指定したidのユーザーが存在しない場合ステータス404を返すこと() throws Exception {
         // ZoneDateTime.nowを固定化する
         ZonedDateTime fixedDateTime = ZonedDateTime.of(
@@ -169,5 +168,120 @@ class UserRestApiIntegrationTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DataSet(value = "datasets/names.yml")
+    @ExpectedDataSet(value = "datasets/updateNames.yml", ignoreCols = "id")
+    @Transactional
+    void 指定したidのユーザーを更新できること() throws Exception {
+        String response = mockMvc.perform(patch("/users/3")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content("""
+                                {
+                                   "name":"takagi"
+                                }
+                                """))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+        JSONAssert.assertEquals("""
+                {
+                   "id":3,
+                   "name":"takagi"
+                }
+                """, response, JSONCompareMode.STRICT);
+    }
+
+    @Test
+    void 更新時のnameがnullか空の場合更新に失敗すること() throws Exception {
+        mockMvc.perform(patch("/users/3")
+                        .content("""
+                                {
+                                   "name":
+                                }
+                                """)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+
+        mockMvc.perform(patch("/users/3")
+                        .content("""
+                                {
+                                   "name":""
+                                }
+                                """)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void 更新時に指定したidのユーザーが存在しない場合ステータス404を返すこと() throws Exception {
+        // ZoneDateTime.nowを固定化する
+        ZonedDateTime fixedDateTime = ZonedDateTime.of(
+                2023, 6, 23, 21, 0, 0, 0,
+                ZoneId.systemDefault());
+
+        // id:99、name:ダミー名を入力
+        try (MockedStatic<ZonedDateTime> mockedStatic = mockStatic(ZonedDateTime.class)) {
+            mockedStatic.when(ZonedDateTime::now).thenReturn(fixedDateTime);
+            String response = mockMvc.perform(MockMvcRequestBuilders.patch("/users/99")
+                            .contentType(MediaType.APPLICATION_JSON_VALUE)
+                            .content("""
+                                    {
+                                       "name":"takagi"
+                                    }
+                                    """))
+                    .andExpect(MockMvcResultMatchers.status().isNotFound())
+                    .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+            JSONAssert.assertEquals("""
+                    {
+                       "timestamp":"2023-06-23T21:00+09:00[Asia/Tokyo]",
+                       "status":"404",
+                       "error":"Not Found",
+                       "message":"No user found for id: 99",
+                       "path":"/users/99"
+                    }
+                    """, response, JSONCompareMode.STRICT);
+        }
+    }
+
+    @Test
+    @DataSet(value = "datasets/names.yml")
+    @ExpectedDataSet(value = "datasets/deleteNames.yml")
+    @Transactional
+    void ユーザーが削除できること() throws Exception {
+        String response = mockMvc.perform(MockMvcRequestBuilders.delete("/users/3"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+        JSONAssert.assertEquals("""
+                {
+                   "message":"successfully deleted user with id : 3"
+                }
+                """, response, JSONCompareMode.STRICT);
+    }
+
+    @Test
+    void 削除時に指定したidのユーザーが存在しない場合ステータス404を返すこと() throws Exception {
+        // ZoneDateTime.nowを固定化する
+        ZonedDateTime fixedDateTime = ZonedDateTime.of(
+                2023, 6, 23, 21, 0, 0, 0,
+                ZoneId.systemDefault());
+
+        try (MockedStatic<ZonedDateTime> mockedStatic = mockStatic(ZonedDateTime.class)) {
+            mockedStatic.when(ZonedDateTime::now).thenReturn(fixedDateTime);
+            String response = mockMvc.perform(MockMvcRequestBuilders.delete("/users/99"))
+                    .andExpect(MockMvcResultMatchers.status().isNotFound())
+                    .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+            JSONAssert.assertEquals("""
+                    {
+                       "timestamp":"2023-06-23T21:00+09:00[Asia/Tokyo]",
+                       "status":"404",
+                       "error":"Not Found",
+                       "message":"No user found for id: 99",
+                       "path":"/users/99"
+                    }
+                    """, response, JSONCompareMode.STRICT);
+        }
     }
 }
