bff02597859933c75f0b3fbc67bd2dd5c773a47c
maki <mkr.fb-66@cameo.plala.or.jp>
2023-07-04 21:21:49 +0900

    CIでの動作確認の為の変更

--- src/main/java/com/example/work9/Work9Application.java
+++ src/main/java/com/example/work9/Work9Application.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Work9Application {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Work9Application.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(Work9Application.class, args);
+    }
 }
