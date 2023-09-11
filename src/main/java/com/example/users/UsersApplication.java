4d83677015d30ee7584175c1eeb82dc3936b7abb
maki <mkr.fb-66@cameo.plala.or.jp>
2023-09-11 15:04:53 +0900

    パッケージ名を命名規則に則って修正

--- src/main/java/com/example/users/UsersApplication.java
+++ src/main/java/com/example/users/UsersApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class usersApplication {
+public class UsersApplication {
     public static void main(String[] args) {
-        SpringApplication.run(usersApplication.class, args);
+        SpringApplication.run(UsersApplication.class, args);
     }
 }
