e75d5d4433ea28ba52031ecb587beb92de309d4f
maki <mkr.fb-66@cameo.plala.or.jp>
2023-09-14 20:38:21 +0900

    homeページ追加

--- userfrontend/src/components/topbar/Topbar.jsx
+++ userfrontend/src/components/topbar/Topbar.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import SearchIcon from '@mui/icons-material/Search';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import "./Topbar.css";
 
 export default function Topbar() {
   return (
@@ -8,14 +12,24 @@ export default function Topbar() {
       </div>
       <div className="topbarCenter">
         <div className="searchbar">
+          <SearchIcon className="searchIcon" />
           <input type="text" className="searchInput" placeholder="探し物はなんですか？" />
         </div>
       </div>
       <div className="topbarRight">
-        <div className="topbarIconItem">1</div>
-        <div className="topbarIconItem">2</div>
+        <div className="topbarIconItems">
+          <div className="topbarIconItem">
+            <ChatIcon />
+            <span className='topbarIconBadge'>1</span>
+          </div>
+          <div className="topbarIconItem">
+            <NotificationsIcon />
+            <span className='topbarIconBadge'>2</span>
+          </div>
+          <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
+        </div>
       </div>
-      <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
+
     </div>
   )
 }
