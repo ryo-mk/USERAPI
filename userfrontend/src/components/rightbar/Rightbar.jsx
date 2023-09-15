dd52b97493a762181253b3d71b3b31177228007a
maki <mkr.fb-66@cameo.plala.or.jp>
2023-09-15 14:04:29 +0900

    フロント実装、profile,login追加

--- userfrontend/src/components/rightbar/Rightbar.jsx
+++ userfrontend/src/components/rightbar/Rightbar.jsx
@@ -4,10 +4,11 @@ import { Users } from "../../dummyData";
 import Online from "../online/Online";
 
 
-export default function Rightbar() {
-  return (
-    <div className='rightbar'>
-      <div className="rightbarWrapper">
+export default function Rightbar({ profile }) {
+
+  const HomeRightbar = () => {
+    return (
+      <>
         <div className="eventContainer">
           <img src="assets/star.png" alt="" className='starImg' />
           <span className="eventText">
@@ -28,7 +29,52 @@ export default function Rightbar() {
         <p className='promotionName'>カーショップ</p>
         <img src="assets/promotion/promotion3.jpeg" alt="" className="rightbarPromotionImg" />
         <p className='promotionName'>shincode株式会社</p>
+      </>
+    );
+  };
+
+  const ProfileRightbar = () => {
+    return (
+      <>
+        <h4 className='rightbarTitle'>ユーザー情報</h4>
+        <div className="rightbarInfo">
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">出身：</span>
+            <span className="rightbarInfoKey">福岡</span>
+          </div>
+          <h4 className='rightbarTitle'>あなたの友達</h4>
+          <div className="rightbarFollowings">
+            <div className="rightbarFollowing">
+              <img src="assets/person/1.jpeg" alt="" className='rightbarFollowingImg' />
+              <span className='rightbarFollowingName'>shincode</span>
+            </div>
+            <div className="rightbarFollowing">
+              <img src="assets/person/2.jpeg" alt="" className='rightbarFollowingImg' />
+              <span className='rightbarFollowingName'>tanaka</span>
+            </div>
+            <div className="rightbarFollowing">
+              <img src="assets/person/3.jpeg" alt="" className='rightbarFollowingImg' />
+              <span className='rightbarFollowingName'>yamaki</span>
+            </div>
+            <div className="rightbarFollowing">
+              <img src="assets/person/4.jpeg" alt="" className='rightbarFollowingImg' />
+              <span className='rightbarFollowingName'>koga</span>
+            </div>
+            <div className="rightbarFollowing">
+              <img src="assets/person/5.jpeg" alt="" className='rightbarFollowingImg' />
+              <span className='rightbarFollowingName'>kikukawa</span>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  };
+
+  return (
+    <div className='rightbar'>
+      <div className="rightbarWrapper">
+        {profile ? <ProfileRightbar /> : <HomeRightbar />}
       </div>
     </div>
-  )
-}
+  );
+};
